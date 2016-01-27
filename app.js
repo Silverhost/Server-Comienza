4cc2d492a8054167a73946b91174d0df44ea5a31
Gonzalo Rios <cuentas@gonzalorios.cl>
2016-01-27 20:12:25 -0300

    cambios en cabeceras

--- app.js
+++ app.js
@@ -15,6 +15,25 @@ var sendgrid  = require('sendgrid')(sendgrid_apikey);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 var router = express.Router();
 
@@ -27,15 +46,15 @@ router.post('/send-email', function(req, res) {
     subject:  'Solicitud de cotización',
     html:     "<h1>Nueva cotización</h1><br />%name% ha solicitado una cotización de %product%<p>El teléfono indicado es %phone%.</p><p>Mensaje: <br />%message%</p>",
     replyto:  req.body.email
-  }
-  
+  };
+
   var email = new sendgrid.Email(email_params);
-  
+
   email.addSubstitution('%name%', req.body.name);
   email.addSubstitution('%product%', req.body.product);
   email.addSubstitution('%phone%', req.body.phone);
   email.addSubstitution('%message%', req.body.message);
-  
+
   sendgrid.send(email, function(err, json) {
     if(err) {
       console.log(err);
@@ -44,9 +63,9 @@ router.post('/send-email', function(req, res) {
       console.log(json);
       res.send(true);
     }
-    
+
   });
-  
+
 });
 
 app.use(router);
